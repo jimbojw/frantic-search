2d77c10c6d22345f8fbe54b6b610c872bf0440a6
jimbojw <wilson.jim.r@gmail.com>
2026-03-12 07:42:50 -0400

    fix: Change Review icon to right arrow

--- app/src/deck-editor/DeckEditorToolbar.tsx
+++ app/src/deck-editor/DeckEditorToolbar.tsx
@@ -96,7 +96,7 @@ export default function DeckEditorToolbar() {
             aria-label="Review changes"
           >
             <svg class="size-3.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
             </svg>
             Review
           </button>
